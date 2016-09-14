fc708ed1b0117150d926d263ad18f007a52f4980
Enrico Werkema <enricowerkema@gmail.com>
2016-09-14 15:49:34 +0200

    Set page foreign keys nullable

--- app/Page.php
+++ app/Page.php
@@ -10,12 +10,6 @@ class Page extends Model
         'path', 'title', 'meta_title', 'meta_desc', 'meta_keywords', 'menu', 'parent_id', 'order', 'header_image_id', 'pagehits',
     ];
 
-    public function setHeader($media)
-    {
-        $this->attributes['header_image_id'] = $media->id;
-        $this->save();
-    }
-
     /**
      * Define relationships.
      */
