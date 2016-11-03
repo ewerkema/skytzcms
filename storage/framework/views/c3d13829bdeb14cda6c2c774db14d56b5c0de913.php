e841fd45ab3dc753d076e5531ed50faf9d57f784
Nirav Panchal <niravp@krishaweb.com>
2016-11-03 17:02:39 +0530

    remove gitignore files

--- storage/framework/views/c3d13829bdeb14cda6c2c774db14d56b5c0de913.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <div class="error-container">
-        <div class="error-content">
-            <div class="error-title"><?php echo e($title); ?></div>
-        </div>
-    </div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('templates.admin.main', ['template' => 'templates.admin.empty'], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
