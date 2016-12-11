ed0af20d46e5525255e1ff3e2c4207572034e81e
Enrico Werkema <enricowerkema@gmail.com>
2016-12-11 23:48:09 +0100

    Bug fix non existing modules on page

--- resources/views/templates/admin/modules/articles.blade.php
+++ resources/views/templates/admin/modules/articles.blade.php
@@ -10,16 +10,20 @@
         </div>
     @else
         <div id="news-{{ $id }}" class="news">
-            <h1>Nieuws: {{ ArticleGroup::find($id)->title }}</h1>
-            @foreach (ArticleGroup::find($id)->articles()->get() as $article)
-                <div class="portlet">
-                    <h3 class="newsheader">{{ $article->title }}</h3>
-                    <p><i>Gepubliceerd: {{ $article->created_at->diffForHumans() }} ({{ $article->created_at->toDateString() }})</i></p>
-                    <p>{{ $article->summary }}</p>
-                    <a href="?article={{ $article->id }}">Lees verder</a>
-                    <hr>
-                </div>
-            @endforeach
+            @if ($articleGroup = ArticleGroup::find($id))
+                <h1>Nieuws: {{ $articleGroup->title }}</h1>
+                @foreach (ArticleGroup::find($id)->articles()->get() as $article)
+                    <div class="portlet">
+                        <h3 class="newsheader">{{ $article->title }}</h3>
+                        <p><i>Gepubliceerd: {{ $article->created_at->diffForHumans() }} ({{ $article->created_at->toDateString() }})</i></p>
+                        <p>{{ $article->summary }}</p>
+                        <a href="?article={{ $article->id }}">Lees verder</a>
+                        <hr>
+                    </div>
+                @endforeach
+            @else
+                <p>Dit nieuwsoverzicht bestaat niet meer / is verwijderd.</p>
+            @endif
         </div>
     @endif
 </div>
--- resources/views/templates/admin/modules/sliders.blade.php
+++ resources/views/templates/admin/modules/sliders.blade.php
@@ -1,8 +1,12 @@
 <div class="slider-wrapper">
     <div id="slider-{{ $id }}" class="slider">
-        @foreach (Slider::find($id)->media()->get() as $image)
-            <div class="slide slide{{ $image->id }}"><img src="{{ $image->photo_url('original') }}" alt="{{ $image->description }}" /></div>
-        @endforeach
+        @if ($slider = Slider::find($id))
+            @foreach ($slider->media()->get() as $image)
+                <div class="slide slide{{ $image->id }}"><img src="{{ $image->photo_url('original') }}" alt="{{ $image->description }}" /></div>
+            @endforeach
+        @else
+            <p>Deze slider bestaat niet meer / is verwijderd.</p>
+        @endif
     </div>
     <div id="slider-direction-nav"></div>
     <div id="slider-control-nav"></div>
