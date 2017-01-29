ab74cefe9d7ef756eb7b6b501caf7766c53ffb70
Enrico Werkema <enricowerkema@gmail.com>
2017-01-29 22:04:26 +0100

    Base styling of article images

--- resources/views/templates/admin/modules/articles.blade.php
+++ resources/views/templates/admin/modules/articles.blade.php
@@ -3,13 +3,29 @@
         <div id="news-{{ $id }}" class="news">
             <h1>Nieuws: {{ $articleGroup->title }}</h1>
             @foreach (ArticleGroup::find($id)->articles()->get() as $article)
-                <div class="portlet">
-                    <h3 class="newsheader">{{ $article->title }}</h3>
-                    <p><i>Gepubliceerd: {{ $article->created_at->diffForHumans() }} ({{ $article->created_at->toDateString() }})</i></p>
-                    <p>{{ $article->summary }}</p>
-                    <a href="?article={{ $article->getSlug() }}">Lees verder</a>
-                    <hr>
-                </div>
+                @if ($article->image_id)
+                    <div class="portlet">
+                        <div class="image small-3">
+                            <a href="?article={{ $article->getSlug() }}"><img src="/{{ Media::find($article->image_id)->path }}" alt=""></a>
+                        </div>
+                        <div class="text small-9">
+                            <h3 class="newsheader">{{ $article->title }}</h3>
+                            <p><i>Gepubliceerd: {{ $article->created_at->diffForHumans() }} ({{ $article->created_at->toDateString() }})</i></p>
+                            <p>{{ $article->summary }}</p>
+                            <a href="?article={{ $article->getSlug() }}">Lees verder</a>
+                            <hr>
+                        </div>
+                    </div>
+                @else
+                    <div class="portlet">
+                        <h3 class="newsheader">{{ $article->title }}</h3>
+                        <p><i>Gepubliceerd: {{ $article->created_at->diffForHumans() }} ({{ $article->created_at->toDateString() }})</i></p>
+                        <p>{{ $article->summary }}</p>
+                        <a href="?article={{ $article->getSlug() }}">Lees verder</a>
+                        <hr>
+                    </div>
+                @endif
+
             @endforeach
         </div>
     @else
--- resources/views/templates/admin/modules/single_article.blade.php
+++ resources/views/templates/admin/modules/single_article.blade.php
@@ -1,6 +1,11 @@
 <div class="portlet">
     <h1>{{ $article->title }}</h1>
     <p><i>Gepubliceerd: {{ $article->created_at->diffForHumans() }} ({{ $article->created_at->toDateString() }})</i></p>
+    @if ($article->image_id)
+        <div class="image">
+            <img src="/{{ Media::find($article->image_id)->path }}" alt="">
+        </div>
+    @endif
     {!! $article->body !!}
     <button href="#" onclick="window.history.back();">Ga terug</button>
 </div>
