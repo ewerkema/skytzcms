c1b68dced0d3b345fee34f79e09fba548275ed18
Enrico Werkema <enricowerkema@gmail.com>
2019-12-18 14:46:49 +0100

    Enable backdrop for modals

--- resources/views/templates/admin/modals/modal.blade.php
+++ resources/views/templates/admin/modals/modal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal {{ isset($animation) ? $animation : 'fade' }}" data-backdrop="{{ isset($backdrop) ? $backdrop : '' }}" tabindex="-1" role="dialog" id="{{ $target }}" aria-labelledby="{{ $target }}">
+<div class="modal {{ isset($animation) ? $animation : 'fade' }}" data-backdrop="{{ isset($backdrop) ? $backdrop : 'true' }}" tabindex="-1" role="dialog" id="{{ $target }}" aria-labelledby="{{ $target }}">
     <div class="modal-dialog {{ (isset($fullscreen) && $fullscreen) ? "modal-full-screen" : "modal-lg" }}" role="document">
         <div class="modal-content">
             <div class="modal-header">
